b0810a7643b7237635ff4e563add4f2d25552f04
Nathan <Natha2020198@live.ytech.edu>
2019-11-13 14:46:59 -0500

    Updated

--- script/index.js
+++ script/index.js
@@ -4,13 +4,23 @@ const DEFAULT_ATK = 5;
 const DEFAULT_DEF = 5;
 const DEFAULT_TEK = 5;
 
-const P0NAME = 'Crash'
-const P0CHARA = 'crashr'
-const P1NAME = 'Sam'
-const P1CHARA = 'saml'
+const P0NAME = "Crash"
+const P0CHARA = "crashr"
+const P1NAME = "Sam"
+const P1CHARA = "saml"
 
 let playerTurn = false;
 let logging = true;
+
+let Player0;
+let Player1;
+
+let gameBox;
+let headerBox;
+let graphicsBox;
+let barsBox;
+let controlsBox;
+let outputBox;
 
 class Fighter {
   constructor(name, charaName) {
@@ -27,7 +37,7 @@ class Fighter {
     this.charaName = charaName;
   }
   attack(target) {
-    console.log(this.name + ' attacked ' + target.name)
+    console.log(this.name + " attacked " + target.name)
   }
   single(target) {
     this.attack(target);
@@ -37,20 +47,23 @@ class Fighter {
     this.attack(target);
   }
   recover() {
-    console.log('Recovered!')
+    console.log("Recovered!")
   }
 }
-
-
-let Player0;
-let Player1;
 
 function startup() {
   Player0 = new Fighter(P0NAME, P0CHARA);
   Player1 = new Fighter(P1NAME, P1CHARA);
 
-  document.getElementById('graphicsBox').innerHTML = '<img src ="img/crashr_idle.png" class ="fighterIMG" alt ="Crash" id ="crashr"></img>'
-  document.getElementById('graphicsBox').innerHTML += '<img src ="img/saml_idle.png" class ="fighterIMG" alt ="Sam" id ="saml"></img>'  
+  gameBox = document.getElementById('gameBox');
+  headerBox = document.getElementById('headerBox');
+  graphicsBox = document.getElementById('graphicsBox');
+  barsBox = document.getElementById('barsBox');
+  controlsBox = document.getElementById('controlsBox');
+  outputBox = document.getElementById('outputBox');
+
+  graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
+  graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
 
   console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
   console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
