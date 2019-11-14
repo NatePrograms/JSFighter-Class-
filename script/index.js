bd4f5f15d5846caf261b758abe324bc07366f541
Harry Nelson <harrisonrnelson@gmail.com>
2019-11-14 08:21:02 -0500

    Commit to merge

--- script/index.js
+++ script/index.js
@@ -66,8 +66,8 @@ function startup() {
   graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
   graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
 
-  console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
-  console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
+  console.log('My name is ' + Player0.name + ' and my ATK is ' + Player0.atk)
+  console.log('My name is ' + Player1.name + ' and my ATK is ' + Player1.atk)
 }
 
 
